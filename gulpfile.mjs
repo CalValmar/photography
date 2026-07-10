c0747e75a62483b438140697f70e924d8454c4a2
CalValmar <vialat.paul@gmail.com>
2026-07-11 00:51:14 +0200

    V2

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,16 +1,20 @@
 import gulp from 'gulp';
-import imageResize from 'gulp-image-resize';
+import sharp from 'sharp';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 import filter from 'gulp-filter';
 import path from 'path';
+import { Transform } from 'stream';
 import del from 'del';
 
 const sass = gulpSass(dartSass);
 const cssSourceGlob = './assets/sass/**/*.scss';
 const cssOutputDir = './assets/css';
+const imageSourceGlob = './images/*.{jpg,jpeg,png,gif,webp}';
+const fullsOutputDir = './images/fulls';
+const thumbsOutputDir = './images/thumbs';
 const generatedCssFiles = [
     `${cssOutputDir}/custom.min.css`,
     `${cssOutputDir}/main.min.css`,
@@ -18,22 +22,54 @@ const generatedCssFiles = [
 ];
 
 gulp.task('delete', function () {
-    return del(['images/*.*']);
+    return del([
+        `${fullsOutputDir}/*.{jpg,jpeg,png,gif,webp}`,
+        `${thumbsOutputDir}/*.{jpg,jpeg,png,gif,webp}`
+    ]);
 });
 
-gulp.task('resize-images', function () {
-    return gulp.src('images/*.*')
-        .pipe(imageResize({
-            width: 1024,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/fulls'))
-        .pipe(imageResize({
-            width: 512,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/thumbs'));
+function resizeTransform(width) {
+    return new Transform({
+        objectMode: true,
+        transform(file, encoding, callback) {
+            if (file.isNull()) {
+                callback(null, file);
+                return;
+            }
+
+            if (file.isStream()) {
+                callback(new Error('Streaming input is not supported for image resizing.'));
+                return;
+            }
+
+            sharp(file.contents)
+                .resize({
+                    width,
+                    withoutEnlargement: true
+                })
+                .toBuffer()
+                .then((buffer) => {
+                    file.contents = buffer;
+                    callback(null, file);
+                })
+                .catch(callback);
+        }
+    });
+}
+
+gulp.task('resize-fulls', function () {
+    return gulp.src(imageSourceGlob)
+        .pipe(resizeTransform(1024))
+        .pipe(gulp.dest(fullsOutputDir));
 });
+
+gulp.task('resize-thumbs', function () {
+    return gulp.src(imageSourceGlob)
+        .pipe(resizeTransform(512))
+        .pipe(gulp.dest(thumbsOutputDir));
+});
+
+gulp.task('resize-images', gulp.parallel('resize-fulls', 'resize-thumbs'));
 
 // clear previously generated css
 gulp.task('clean-css', function () {
